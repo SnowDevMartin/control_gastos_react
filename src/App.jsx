c4eb7c52c81f9e3bef0ddac6111a432780fa4936
SnowDevMartin <electronicafr21@gmail.com>
2023-09-01 14:31:26 +0200

    add. creando nuevos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,77 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+
+
+
+
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  )
+
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  )
+
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+
+  const [gastoEditar, setGastoEditar] = useState({})
+
+  const [filtro, setFiltro] = useState('')
+  const [gastosFiltrados, setGastosFiltrados] = useState([])
 
   return (
-    <>
-      
-    </>
+    <div className={modal ? 'fijar' : '' }>
+      <Header 
+          gastos={gastos}
+          setGastos={setGastos}
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          isValidPresupuesto={isValidPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <>
+          <main>
+            <Filtros 
+              filtro={filtro}
+              setFiltro={setFiltro}
+            />
+            <ListadoGastos 
+                gastos={gastos}
+                setGastoEditar={setGastoEditar}
+                eliminarGasto={eliminarGasto}
+                filtro={filtro}
+                gastosFiltrados={gastosFiltrados}
+            />
+          </main>
+          <div className="nuevo-gasto">
+              <img 
+                  src={IconoNuevoGasto}
+                  alt="icono nuevo gasto"
+                  onClick={handleNuevoGasto}
+              />
+          </div>
+        </>
+      )}
+
+      {modal && <Modal 
+                  setModal={setModal}
+                  animarModal={animarModal}
+                  setAnimarModal={setAnimarModal}
+                  guardarGasto={guardarGasto}
+                  gastoEditar={gastoEditar}
+                  setGastoEditar={setGastoEditar}
+                />}
+
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,36 @@
-import ControlPresupuesto from "./ControlPresupuesto"
-import NuevoPresupuesto from "./NuevoPresupuesto"
+import NuevoPresupuesto from './NuevoPresupuesto'
+import ControlPresupuesto from './ControlPresupuesto'
 
 const Header = ({ 
+    gastos,
+    setGastos,
     presupuesto, 
     setPresupuesto, 
     isValidPresupuesto, 
     setIsValidPresupuesto 
 }) => {
-  return (
-    <header>
-        <h1>Planificador de Gastos</h1>
-        {isValidPresupuesto ? (
-            <ControlPresupuesto 
-                presupuesto={presupuesto}
-            />
-        ) : (
-            <>
-            <NuevoPresupuesto
-            presupuesto={presupuesto}
-            setPresupuesto={setPresupuesto}
-            setIsValidPresupuesto={setIsValidPresupuesto}
-            />
-            </>
-        )
-        }
-        
-    </header>
-  )
+    return (
+        <header>
+            <h1>Planificador de Gastos</h1>
+
+            {isValidPresupuesto ? (
+                <ControlPresupuesto 
+                    gastos={gastos}
+                    setGastos={setGastos}
+                    presupuesto={presupuesto}
+                    setPresupuesto={setPresupuesto}
+                    setIsValidPresupuesto={setIsValidPresupuesto}
+                />
+            ) : (
+                <NuevoPresupuesto 
+                    presupuesto={presupuesto}
+                    setPresupuesto={setPresupuesto}
+                    setIsValidPresupuesto={setIsValidPresupuesto }
+                />
+            )}
+
+        </header>
+    )
 }
 
 export default Header
